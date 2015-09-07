363eedb75462d118d9be6715d2b531d8031e1ead
jsonpeter <aspcms@foxmail.com>
2015-09-07 15:21:36 +0800

    mysql

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,10 @@
+//var crypto=require('crypto'); //加密模块
 $(function(){
     $("#btn_login").click(function(){
         $.post("/login",{name:$("#username").val(),pwd:$("#userpwd").val()},function(data){
-             alert(data.msg);
+             if(data.status==0){
+                 alert("登录成功！");
+             }
         })
     })
 });
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var crypto=require('crypto'); //加密模块
+var sqlback=require('../database/conn');
 var router = express.Router();
 
 /* GET home page. */
@@ -10,9 +11,22 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: '用户登录',message:'Node.js+Express服务端用户登录' });
 });
 router.post('/login', function(req, res) {
-    var sha1 = crypto.createHash('sha1');
-    sha1.update(req.body.name);
-    sha1.update(req.body.pwd);
-    res.json({"status":0,"msg":"用户名："+req.body.name+" 密码："+req.body.pwd+"\nkey签名：\n"+sha1.digest('hex')});
+    var status=-1,message="";
+    var name=req.body.name;
+    var pwd=req.body.pwd;
+    var sha1 = crypto.createHash('md5');
+    sha1.update(pwd);
+    pwd=sha1.digest('hex');
+    var _sql="SELECT * FROM wp_users WHERE user_login='"+name+"' and user_pass='"+pwd+"'";
+    sqlback("SELECT * FROM wp_users",function(data){
+        if(data.length>0){
+            status=0;
+            message="登录成功！";
+        }
+     //for(var i in data){
+     //    console.log("----"+data[i].user_pass);
+     // }
+    });
+    res.json({"status":status,"msg":message});
 });
 module.exports = router;
